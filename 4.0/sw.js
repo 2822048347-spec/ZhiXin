aa3ebe575dc80be5515ea391f680442930dbba09
2822048347-spec <2822048347-spec@users.noreply.github.com>
2026-06-23 16:00:24 +0800

    修复3.0 PWA：修复manifest start_url + 添加sw.js

---
+++ 4.0/sw.js
@@ -0,0 +1,30 @@
+// Service Worker for ZhiXin 4.0
+const CACHE = 'zhixin-4.0-v1';
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll([
+      './',
+      './index.html',
+      './manifest.json',
+      './icon-192.png',
+      './icon-512.png'
+    ]))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(
+      keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+    ))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
